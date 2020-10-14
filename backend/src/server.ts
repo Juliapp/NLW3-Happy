20597c8891154e8c38cf5aea8888ef0103b97ce0
Juliapp <aragaopintojuli@gmail.com>
2020-10-14 13:38:06 -0300

    :tada: Api Finalizada

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,11 +3,13 @@ import 'express-async-errors';
 import path from 'path';
 import './database/connection';
 import errorHandler from './errors/handler';
-//import cors from 'cors';
+import cors from 'cors';
 
 import routes from './routes';
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
