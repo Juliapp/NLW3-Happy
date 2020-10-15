f57e14abbe28a7f11c2df7bffcd6af2e7d2661b0
Juliapp <aragaopintojuli@gmail.com>
2020-10-15 12:07:53 -0300

    :art: Cadastro de novo orfanato quase pronto

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 import { Map, Marker, TileLayer } from 'react-leaflet';
 
 import { FiPlus } from 'react-icons/fi';
@@ -6,42 +6,101 @@ import { FiPlus } from 'react-icons/fi';
 import '../styles/pages/create-orphanage.css';
 import Sidebar from '../components/Sidebar';
 import mapIcon from '../utils/mapIcon';
+import { latLng, LeafletMouseEvent } from 'leaflet';
 
 export default function CreateOrphanage() {
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+  const [mapArea, setMapArea] = useState({
+    latitude: -12.2546377,
+    longitude: -38.9796742,
+  });
+
+  const handleMapClick = (event: LeafletMouseEvent) => {
+    const { lat, lng } = event.latlng;
+    setPosition({ latitude: lat, longitude: lng });
+  };
+
+  const [name, setName] = useState('');
+  const [about, setAbout] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [opening_hours, setOpeningHours] = useState('');
+  const [open_on_weekends, setOpenOnWeekends] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setMapArea({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  const handleOnSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    const { latitude, longitude } = position;
+    console.log({
+      name,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      latitude,
+      longitude,
+    });
+  };
+
   return (
     <div id="page-create-orphanage">
       <main>
         <Sidebar />
-        <form className="create-orphanage-form">
+        <form className="create-orphanage-form" onSubmit={handleOnSubmit}>
           <fieldset>
             <legend>Dados</legend>
 
             <Map
-              center={[-27.2092052, -49.6401092]}
+              center={[mapArea.latitude, mapArea.longitude]}
               style={{ width: '100%', height: 280 }}
               zoom={15}
+              onclick={handleMapClick}
             >
               <TileLayer
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker
+              {position.latitude !== 0 && (
+                <Marker
+                  interactive={false}
+                  icon={mapIcon}
+                  position={[position.latitude, position.longitude]}
+                />
+              )}
+
+              {/* <Marker
                 interactive={false}
                 icon={mapIcon}
                 position={[-27.2092052, -49.6401092]}
-              />
+              /> */}
             </Map>
 
             <div className="input-block">
               <label htmlFor="name">Nome</label>
-              <input id="name" />
+              <input
+                id="name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="about">
                 Sobre <span>Máximo de 300 caracteres</span>
               </label>
-              <textarea id="name" maxLength={300} />
+              <textarea
+                id="about"
+                maxLength={300}
+                value={about}
+                onChange={(event) => setAbout(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
@@ -49,7 +108,7 @@ export default function CreateOrphanage() {
 
               <div className="uploaded-image"></div>
 
-              <button className="new-image">
+              <button className="new-image" type="button">
                 <FiPlus size={24} color="#15b6d6" />
               </button>
             </div>
@@ -60,22 +119,42 @@ export default function CreateOrphanage() {
 
             <div className="input-block">
               <label htmlFor="instructions">Instruções</label>
-              <textarea id="instructions" />
+              <textarea
+                id="instructions"
+                value={instructions}
+                onChange={(event) => setInstructions(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
-              <label htmlFor="opening_hours">Nome</label>
-              <input id="opening_hours" />
+              <label htmlFor="opening_hours">Horário de funcionamento</label>
+              <input
+                id="opening_hours"
+                value={opening_hours}
+                onChange={(event) => setOpeningHours(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="open_on_weekends">Atende fim de semana</label>
 
               <div className="button-select">
-                <button type="button" className="active">
+                <button
+                  type="button"
+                  className={open_on_weekends ? 'active' : ''}
+                  onClick={() => setOpenOnWeekends(true)}
+                >
                   Sim
                 </button>
-                <button type="button">Não</button>
+                <button
+                  onClick={() => {
+                    setOpenOnWeekends(false);
+                  }}
+                  className={!open_on_weekends ? 'active' : ''}
+                  type="button"
+                >
+                  Não
+                </button>
               </div>
             </div>
           </fieldset>
